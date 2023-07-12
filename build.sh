400013c5d4b2a172619e20bd4bccda161501f48c
Gratien Dhaese <gratien.dhaese@gmail.com>
2023-07-12 14:13:25 +0200

    #4 update graphite container image

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 # Build the graphite container image
+#
+# To find the current used version number use command:
+# $ kubectl describe pod -n graphite  graphite-0 | grep -i image:
+#   Image:          ghcr.io/gdha/graphite:v1.2
+# To build a newer version use: ./build.sh v1.3  (example)
 REL=${1:-v1.0}
 cat ~/.ghcr-token | docker login ghcr.io -u gdha --password-stdin
 echo "Building graphite:$REL"
